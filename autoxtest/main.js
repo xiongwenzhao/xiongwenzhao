3c51e193eb6619da8d849c6a192b40795fb99f9d
xwz <877958448@qq.com>
2025-04-01 16:02:54 +0800

    优化滑动逻辑，调整随机滑动时长范围，添加重启应用和结束应用功能

--- autoxtest/main.js
+++ autoxtest/main.js
@@ -3,8 +3,11 @@ auto.waitFor();
 var width = device.width;
 var height = device.height;
  //设置随机滑动时长范围
- var timeMin=1000
- var timeMax=3000
+ var timeMin=700
+ var timeMax=1000
+ //设置控制点极限距离
+ var leaveHeightLength=100
+
 log("屏幕宽度: " + width + ", 屏幕高度: " + height);
 setScreenMetrics(width, height)
 // 任务队列
@@ -32,19 +35,82 @@ function processTasks() {
 // 定时器定期检查任务队列
 setInterval(processTasks, 1000);
 
+
+
+
 // 示例：添加任务
 addTask(() => {
-    randomSwipe(100, 200, 300, 400);
-}, new Date().getTime() + 5000); // 5秒后执行
+    // restart("MT管理器");
+    swipe(false);
+}, new Date().getTime() + 1); // 5秒后执行
 
-addTask(() => {
-    randomSwipe(400, 500, 600, 700);
-}, new Date().getTime() + 10000); // 10秒后执行
+// addTask(() => {
+//     randomSwipe(400, 500, 600, 700);
+// }, new Date().getTime() + 10000); // 10秒后执行
 
-addTask(() => {
-    
-    text("Chrome").findOne(1000).click();
-}, new Date().getTime() + 1); // 10秒后执行
+// addTask(() => {
+//     text("Chrome").findOne(1000).click();
+// }, new Date().getTime() + 1); // 10秒后执行
+
+
+function swipe(b){
+    let x1 = width/2+random(0, width/3);
+    let x2 = width/2+random(0, width/3);
+    let y1;
+    let y2;
+    if (b) {
+        y1 = random(height/5*4, height);
+        y2 = random(height/5*2, height/5*3);
+    } else {
+        y1 = random(height/5*2, height/5*3);
+        y2 = random(height/5*4, height);
+    }
+    randomSwipe(x1, y1, x2, y2);
+}
+
+function restart(name){
+    log("重启软件执行");
+    killApp(name);//结束
+    sleep(1000);
+    launchApp(name);//启动
+    let packageName = app.getPackageName(name);
+    while(packageName!= currentPackage()){ 
+        log("等待软件启动中...");
+        sleep(1000);
+    }
+    log("软件已启动");
+}
+
+function killApp(name) {
+    let forcedStopStr = ["强", "结束"];
+    let packageName = app.getPackageName(name);
+    if (packageName) {
+        app.openAppSetting(packageName);
+        text(name).waitFor();
+        for (var i = 0; i < forcedStopStr.length; i++) {
+            if (textContains(forcedStopStr[i]).exists()) {
+                let forcedStop = textContains(forcedStopStr[i]).findOne();
+                if (forcedStop.enabled()) {
+                    forcedStop.click();
+                    text("确定").findOne().click();
+                    log(name + "已结束运行");
+                    sleep(800);
+                    back();
+                    break;
+                } else {
+                    log(name + "不在后台运行！");
+                    back();
+                    break;
+                }
+            }
+        }
+    } else {
+        log("应用不存在");
+    }
+    sleep(1000);
+    home();
+    log("返回桌面");
+}
 
 /**
  * 四点生成贝塞尔曲线
@@ -104,10 +170,6 @@ function bezierCreate(x1,y1,x2,y2,x3,y3,x4,y4){
  * 效果：模拟真人滑动
  */
 function randomSwipe(sx,sy,ex,ey){
-   
-    //设置控制点极限距离
-    var leaveHeightLength=500
-    
     if (Math.abs(ex - sx) > Math.abs(ey - sy)) {
         var my = (sy + ey) / 2;
         var y2 = my + random(0, leaveHeightLength);
@@ -128,6 +190,6 @@ function randomSwipe(sx,sy,ex,ey){
 
     var time=[0,random(timeMin,timeMax)]
     var track=bezierCreate(sx,sy,x2,y2,x3,y3,ex,ey)
-    log("滑动:", new Date()+"控制点A坐标："+x2+","+y2+"控制点B坐标："+x3+","+y3+"滑动时长："+time[1])
+    log("滑动:", new Date()+"Start坐标："+sx+","+sy+"end坐标："+ex+","+ey+"滑动时长："+time[1])
     gestures(time.concat(track))
 }
